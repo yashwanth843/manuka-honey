dfda68e205ecc7816a35ce9e958a1d0f5b0e78df
yashwanth <yashwanth.kinnu123@gmail.com>
2025-08-31 18:32:48 +0530

    updates

--- src/components/FooterPage/index.js
+++ src/components/FooterPage/index.js
@@ -1,39 +1,4 @@
-// import React from 'react'
-// import { RiInformationLine } from "react-icons/ri";
-// import "./index.css"
 
-// const FooterPage = () => {
-//   return (
-//     <div className='footerpage-container'>
-//         <div className='aboutproductContainer'>
-//             <img src='https://res.cloudinary.com/dlaypemls/image/upload/v1756637644/logo_jgjhwr.png' alt='logo' className='logo'/>
-//             <p className='logo-text'>Colourclub members earn up to <span className='number'>56</span> reward points when buy this item. sign up or log in</p>
-//         </div>
-//         <div className='delivery-details-container'>
-//             <div className='delivery-part-container'>
-//                 <h4 className='delivery-text'>DELIVERY</h4>
-//                 <p className='delivery-para'>FREE DELIVERY ON ORDERS OVERS $30</p>
-//             </div>
-
-//             <div className='delivery-part-container'>
-//                 <h4 className='delivery-text'>ESTIMATED DELIVERY DATE:</h4>
-//                 <p className='delivery-para'>Jun 9 - Jun 13 to</p>
-//             </div>
-//             <hr/>
-//         </div>
-//         <div className='delivery-details-container'>
-//             <div className='delivery-part-container'>
-//                 <h4 className='delivery-text'>AFTER PAY</h4>
-//                 <p className='delivery-para'>or 4 interest-free payments of $13.97 with 
-//                     <img src="https://res.cloudinary.com/dlaypemls/image/upload/v1756638969/button_logo_keprxj.png" alt="log" className='logo-btn'/><RiInformationLine/></p>
-//             </div>
-//             <hr/>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default FooterPage
 import React, { useState } from 'react';
 import { RiInformationLine } from "react-icons/ri";
 import "./index.css";
@@ -43,15 +8,15 @@ const FooterPage = () => {
 
   return (
     <div className='footerpage-container'>
-      {/* Product Info */}
+
       <div className='aboutproductContainer'>
-        <img src='https://res.cloudinary.com/dlaypemls/image/upload/v1756637644/logo_jgjhwr.png' alt='logo' className='logo'/>
+        <img src='https://res.cloudinary.com/dlaypemls/image/upload/v1756637644/logo_jgjhwr.png' alt='logo' className='logo' />
         <p className='logo-text'>
           Colourclub members earn up to <span className='number'>56</span> reward points when buy this item. Sign up or log in
         </p>
       </div>
 
-      {/* Delivery Info */}
+
       <div className='delivery-details-container'>
         <div className='delivery-part-container'>
           <h4 className='delivery-text'>DELIVERY</h4>
@@ -62,36 +27,36 @@ const FooterPage = () => {
           <h4 className='delivery-text'>ESTIMATED DELIVERY DATE:</h4>
           <p className='delivery-para'>Jun 9 - Jun 13 to</p>
         </div>
-        <hr/>
+        <hr />
       </div>
 
-      {/* AfterPay */}
+
       <div className='delivery-details-container'>
         <div className='delivery-part-container'>
           <h4 className='delivery-text'>AFTER PAY</h4>
           <p className='delivery-para'>
-            or 4 interest-free payments of $13.97 with 
-            <img src="https://res.cloudinary.com/dlaypemls/image/upload/v1756638969/button_logo_keprxj.png" alt="log" className='logo-btn'/>
-            <RiInformationLine/>
+            or 4 interest-free payments of $13.97 with
+            <img src="https://res.cloudinary.com/dlaypemls/image/upload/v1756638969/button_logo_keprxj.png" alt="log" className='logo-btn' />
+            <RiInformationLine />
           </p>
         </div>
-        <hr/>
+        <hr />
       </div>
 
-      {/* ✅ UMF SCALE */}
+
       <div className="umf-scale-container">
         <h4 className="scale-title">UMF™ SCALE</h4>
         <div className="umf-options">
-          <span className={`umf-option orange ${activeUMF==="10" ? "active" : ""}`} onClick={() => setActiveUMF("10")}>UMF™ 10+</span>
-          <span className={`umf-option red ${activeUMF==="15" ? "active" : ""}`} onClick={() => setActiveUMF("15")}>UMF™ 15+</span>
-          <span className={`umf-option pink ${activeUMF==="20" ? "active" : ""}`} onClick={() => setActiveUMF("20")}>UMF™ 20+</span>
-          <span className={`umf-option purple ${activeUMF==="24" ? "active" : ""}`} onClick={() => setActiveUMF("24")}>UMF™ 24+</span>
-          <span className={`umf-option skyblue ${activeUMF==="26" ? "active" : ""}`} onClick={() => setActiveUMF("26")}>UMF™ 26+</span>
-          <span className={`umf-option green ${activeUMF==="28" ? "active" : ""}`} onClick={() => setActiveUMF("28")}>UMF™ 28+</span>
-          <span className={`umf-option blue ${activeUMF==="30" ? "active" : ""}`} onClick={() => setActiveUMF("30")}>UMF™ 30+</span>
+          <span className={`umf-option orange ${activeUMF === "10" ? "active" : ""}`} onClick={() => setActiveUMF("10")}>UMF™ 10+</span>
+          <span className={`umf-option red ${activeUMF === "15" ? "active" : ""}`} onClick={() => setActiveUMF("15")}>UMF™ 15+</span>
+          <span className={`umf-option pink ${activeUMF === "20" ? "active" : ""}`} onClick={() => setActiveUMF("20")}>UMF™ 20+</span>
+          <span className={`umf-option purple ${activeUMF === "24" ? "active" : ""}`} onClick={() => setActiveUMF("24")}>UMF™ 24+</span>
+          <span className={`umf-option skyblue ${activeUMF === "26" ? "active" : ""}`} onClick={() => setActiveUMF("26")}>UMF™ 26+</span>
+          <span className={`umf-option green ${activeUMF === "28" ? "active" : ""}`} onClick={() => setActiveUMF("28")}>UMF™ 28+</span>
+          <span className={`umf-option blue ${activeUMF === "30" ? "active" : ""}`} onClick={() => setActiveUMF("30")}>UMF™ 30+</span>
         </div>
 
-        {/* ✅ Taste Profile */}
+
         <h4 className="scale-title">TASTE PROFILE</h4>
         <div className="taste-bar">
           <span className="taste-left">Clean & Intense</span>
